81bb42994efa26d8cb8e968c2045d54e98c9d15a
Ali Altintas <altintas.ali@gmail.com>
2023-01-06 13:17:29 +0100

    added example

--- R/create.boot.FUN.mean.R
+++ R/create.boot.FUN.mean.R
@@ -6,20 +6,30 @@
 #' @param contrast.frm  a string formula specifying the names of the predictors over which emmeans are desired.
 #' @param pairwise If TRUE (default), test for differential rhythmicity (compare groups). If FALSE, test for rhythmicity only (detect rhythmicity)
 #'
-#' @return
+#' @return function input for [bootMer] calculating the mean of MESOR, Amplitude and Acrophase
+#'
+#' @import lme4
 #' @export
 #'
 #' @examples
-#' f1.b<-lmer(hrv~age+gender+T0toT14+Tneg7toT7+
-#'            gender*rrr+gender*sss+
-#'            T0toT14*rrr+T0toT14*sss+
-#'            Tneg7toT7*rrr+Tneg7toT7*sss+
-#'            (1|participant_id),
-#'            data=db.model, na.action = na.omit)
+#' \dontrun{
+#' #' data(db.cosinor)
+#' head(db.cosinor)
+#' ## create cosinor paramenters needed for cosinor model
+#' db.model<-create.cosinor.param(time="Hour_of_Day", period=24, data=db.cosinor)
 #'
-#' boot.mean<-bootMer(f1.b,FUN = create.boot.FUN.mean(contrast.frm='~T0toT14+Tneg7toT7'), nsim =500, parallel = "multicore", ncpus=8)
+#' ## examine the effects of gender on MESOR, acrophase, amplitude on hrv
+#' f <- fit.cosinor.mixed(y = "hrv",
+#'                        x = "gender",
+#'                        random = "1|participant_id",
+#'                        data = db.model)
 #'
-#'
+#' boot.mean <- lme4::bootMer(f,
+#'                            FUN = create.boot.FUN.mean(contrast.frm='~gender'),
+#'                            nsim = 500,
+#'                            parallel = "multicore",
+#'                            ncpus = 8)
+#' }
 create.boot.FUN.mean<-function(contrast.frm, pairwise = TRUE){
   boot.FUN.mean=function(.){just.get.means.cosinor(., contrast.frm=contrast.frm, pairwise = pairwise)};
   return(boot.FUN.mean)}
