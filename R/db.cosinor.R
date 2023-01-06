6cfc83c7e3781b1534f30efc5a1592e54e7f4031
Ali Altintas <altintas.ali@gmail.com>
2023-01-06 13:18:44 +0100

    added info for the package data

---
+++ R/db.cosinor.R
@@ -0,0 +1,7 @@
+#' @name db.cosinor
+#' @title Cosinor dataset from cosinoRmixedeffects
+#' @description COVID data from cosinoRmixedeffects
+#' @docType data
+#' @usage db.cosinor
+#' @format \code{data.frame}
+NULL
